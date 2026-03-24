20adafa400cf10ae9e68b774c2522aa46280f179
Mahek-01001 <87099422+Mahek-01001@users.noreply.github.com>
2026-03-24 21:19:43 +0530

    Update rdf_generator.py

--- pipeline/rdf_generator.py
+++ pipeline/rdf_generator.py
@@ -1,4 +1,4 @@
-from rdflib import Graph, URIRef, Namespace
+from rdflib import Graph, URIRef, Namespace, RDF
 from pipeline.normalizer import normalize
 from pipeline.mapper import map_predicate
 
@@ -8,9 +8,16 @@ DBO = Namespace("http://dbpedia.org/ontology/")
 def generate_rdf(data):
     g = Graph()
 
-    
     g.bind("dbo", DBO)
 
+    
+    if "entities" in data:
+        for ent in data["entities"]:
+            subject = URIRef(EX[normalize(ent["name"])])
+            obj = URIRef(DBO[normalize(ent["type"])])
+            g.add((subject, RDF.type, obj))
+
+    # Existing relation logic
     for rel in data["relations"]:
         if rel.get("confidence", 1) < 0.7:
             continue
